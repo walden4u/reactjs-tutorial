d861d9dd16b8cd126ce78c2a6a2219419f088099
walden4u <(duldulnet@gmail.com)>
2022-05-10 21:50:53 +0900

    5_1

--- hello-world/src/components/Greet.js
+++ hello-world/src/components/Greet.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
-function Greet() {
-    return (        
-        <h1>Hello Vishwa</h1>
-    );
-}
+// function Greet() {
+//     return (        
+//         <h1>Hello Vishwa</h1>
+//     );
+// }
+
+const Greet = () => <h1>Hello Vishwa</h1>;
 
 export default Greet;
